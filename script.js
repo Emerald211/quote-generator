089580639281e0d03bdbead46c73e8871239bae4
Emerald211 <aderibigbeemmanuel2004@gmail.com>
2022-04-08 06:38:51 -1200

    quote generator

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ const quoteText = document.getElementById ('quote');
 const authorText = document.getElementById ('author');
 const twitterBtn = document.getElementById ('twitter');
 const newQuoteBtn = document.getElementById ('new-quote');
+const loader = document.getElementById('load');
 
+
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+
+}
+
+function complete() {
+    quoteContainer.hidden = false
+    loader.hidden = true
+}
 
 
 let apiQuotes = [];
@@ -11,6 +24,8 @@ let apiQuotes = [];
 
 // Pick a random quote from the api
 function newQuote() {
+    loading();
+
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
     // Check author if not there replace with "Unknown"
     if (!quote.author) {
@@ -23,12 +38,17 @@ function newQuote() {
 
     
 
-
+    
     quoteText.textContent = quote.text;
+    complete();
+    
 }
 
 
+
+
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
@@ -37,11 +57,23 @@ async function getQuotes() {
     } catch (error) {
         // Catch error here
     }
-
 }
+
+function tweetQuote() {
+    const quote = quoteText.innerText
+    const author = authorText.innerText
+    const twitterUrl = `http://twitter.com/intent/tweet?text=${quote} - author=${author}`
+    window.open(twitterUrl, "_blank")
+    
+}
+
+twitterBtn.addEventListener ('click', tweetQuote)
+
 
 // On load
 
 getQuotes(); 
+
+
 
 newQuoteBtn.addEventListener("click", getQuotes)
